5736dac2beadb7a54ad8275bfd43b94e048d5b4d
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-07-28 17:11:39 -0400

    added relevant files and started building components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
-
+import Header from "./Header"
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>NASA</h1>
+            <h2>Photo Of The Day</h2>
+        </div>
+    )
+}
+
+export default Header;
